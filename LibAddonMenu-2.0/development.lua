9701819bb6c07f8c0554d5734ee728a2d46636aa
sirinsidiator <insidiator@cmos.at>
2016-07-12 16:18:31 +0200

    implemented #46

---
+++ LibAddonMenu-2.0/development.lua
@@ -0,0 +1,3 @@
+-- this file is only used for the github version to prevent accidental bundling of a copy with r999.
+-- see https://github.com/sirinsidiator/ESO-LibAddonMenu/issues/46
+_LAM2_VERSION_NUMBER = 999
