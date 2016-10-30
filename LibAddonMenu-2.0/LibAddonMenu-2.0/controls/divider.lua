78934aa88614513ec9885257b38937f6861702a9
sirinsidiator <insidiator@cmos.at>
2016-10-30 16:37:23 +0100

    fixed #69

--- LibAddonMenu-2.0/LibAddonMenu-2.0/controls/divider.lua
+++ LibAddonMenu-2.0/LibAddonMenu-2.0/controls/divider.lua
@@ -7,29 +7,34 @@
 } ]]
 
 
-local widgetVersion = 1
+local widgetVersion = 2
 local LAM = LibStub("LibAddonMenu-2.0")
 if not LAM:RegisterWidget("divider", widgetVersion) then return end
 
 local wm = WINDOW_MANAGER
 
 local MIN_HEIGHT = 10
+local MAX_HEIGHT = 50
+local MIN_ALPHA = 0
+local MAX_ALPHA = 1
 local DEFAULT_ALPHA = 0.25
+
+local function GetValueInRange(value, min, max, default)
+    if not value or type(value) ~= "number" then
+        return default
+    end
+    return math.min(math.max(min, value), max)
+end
+
 function LAMCreateControl.divider(parent, dividerData, controlName)
     local control = LAM.util.CreateBaseControl(parent, dividerData, controlName)
     local isHalfWidth = control.isHalfWidth
     local width = control:GetWidth()
-    local height = dividerData.height
-    if not height or type(height) ~= "numeric" or height < MIN_HEIGHT then
-        height = MIN_HEIGHT
-    end
+    local height = GetValueInRange(dividerData.height, MIN_HEIGHT, MAX_HEIGHT, MIN_HEIGHT)
+    local alpha = GetValueInRange(dividerData.alpha, MIN_ALPHA, MAX_ALPHA, DEFAULT_ALPHA)
+
     control:SetDimensions(isHalfWidth and width / 2 or width, height)
 
-    
-    local alpha = dividerData.alpha
-    if not alpha or type(alpha) ~= "numeric" or alpha > 1 or alpha < 0 then
-        alpha = DEFAULT_ALPHA
-    end
     control.divider = wm:CreateControlFromVirtual(nil, control, "ZO_Options_Divider")
     local divider = control.divider
     divider:SetWidth(isHalfWidth and width / 2 or width)
