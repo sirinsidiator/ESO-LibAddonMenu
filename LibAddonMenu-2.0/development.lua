ea219d130fb7b905c37ce141921ecea321b738e2
sirinsidiator <insidiator@cmos.at>
2019-04-06 14:45:33 +0200

    added slash command to hook into LAM callbacks during development

--- LibAddonMenu-2.0/development.lua
+++ LibAddonMenu-2.0/development.lua
@@ -1,3 +1,31 @@
 -- this file is only used for the github version to prevent accidental bundling of a copy with r999.
 -- see https://github.com/sirinsidiator/ESO-LibAddonMenu/issues/46
 _LAM2_VERSION_NUMBER = 999
+
+local debugging = false
+SLASH_COMMANDS["/lamdebug"] = function()
+    if(debugging) then return end
+    debugging = true
+
+    local cm = CALLBACK_MANAGER
+    local LAM = LibStub("LibAddonMenu-2.0")
+
+    local function GetPanelName(control)
+        local panel = LAM.util.GetTopPanel(control)
+        return panel.label:GetText()
+    end
+
+    local CALLBACKS = {
+        "LAM-RefreshPanel",
+        "LAM-PanelOpened",
+        "LAM-PanelClosed",
+        "LAM-PanelControlsCreated",
+    }
+
+    for i = 1, #CALLBACKS do
+        local callbackName = CALLBACKS[i]
+        cm:RegisterCallback(callbackName, function(control) df("[LAM] %s: %s", callbackName, GetPanelName(control)) end)
+    end
+
+    d("[LAM] Debug hooks enabled")
+end
