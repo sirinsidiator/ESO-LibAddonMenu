2deb1d989286c4afeffbe2128ba990a630e1f3df
Baertram <pz1@gmx.net>
2020-11-10 18:27:35 +0100

    Update custom widget to version 8
    -Added possible callback function createFunc -> Called as the controlw as created and designed to e.g. properly attach other controls etc. to the custom control
    -Added possible parameters minHeight and maxHeight

--- LibAddonMenu-2.0/LibAddonMenu-2.0/controls/custom.lua
+++ LibAddonMenu-2.0/LibAddonMenu-2.0/controls/custom.lua
@@ -1,11 +1,14 @@
 --[[customData = {
     type = "custom",
     reference = "MyAddonCustomControl", -- unique name for your control to use as reference (optional)
+    createFunc = function(customControl) end, -- function to call when this custom control was created (optional)
     refreshFunc = function(customControl) end, -- function to call when panel/controls refresh (optional)
     width = "full", -- or "half" (optional)
+    minHeight = function() return db.minHeightNumber end, --or number for the minimum height of this control. Default: 26 (optional)
+    maxHeight = function() return db.maxHeightNumber end, --or number for the maximum height of this control. Default: 4 * minHeight (optional)
 } ]]
 
-local widgetVersion = 7
+local widgetVersion = 8
 local LAM = LibAddonMenu2
 if not LAM:RegisterWidget("custom", widgetVersion) then return end
 
@@ -16,20 +19,25 @@ local function UpdateValue(control)
 end
 
 local MIN_HEIGHT = 26
+
 function LAMCreateControl.custom(parent, customData, controlName)
     local control = LAM.util.CreateBaseControl(parent, customData, controlName)
     local width = control:GetWidth()
     control:SetResizeToFitDescendents(true)
 
+    local minHeight = (control.data.minHeight and LAM.util.GetDefaultValue(control.data.minHeight)) or MIN_HEIGHT
+    local maxHeight = (control.data.maxHeight and LAM.util.GetDefaultValue(control.data.maxHeight)) or (minHeight * 4)
+
     if control.isHalfWidth then --note these restrictions
-        control:SetDimensionConstraints(width / 2, MIN_HEIGHT, width / 2, MIN_HEIGHT * 4)
+        control:SetDimensionConstraints(width / 2, minHeight, width / 2, maxHeight)
     else
-        control:SetDimensionConstraints(width, MIN_HEIGHT, width, MIN_HEIGHT * 4)
+        control:SetDimensionConstraints(width, minHeight, width, maxHeight)
     end
 
     control.UpdateValue = UpdateValue
 
     LAM.util.RegisterForRefreshIfNeeded(control)
 
+    if customData.createFunc then customData.createFunc(control) end
     return control
 end
